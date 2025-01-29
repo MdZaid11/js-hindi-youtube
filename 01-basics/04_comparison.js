c4e2576cbb0f2a50a26ebb6430a8e4d06fd28283
MdZaid11 <109697860+MdZaid11@users.noreply.github.com>
2025-01-29 20:02:10 +0000

    Comparison

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1)  // "2" ---> converted to number
+console.log("02">1)
+
+console.log(null>0);
+// console.log(null==0);
+console.log(null>=0)
+
+
+// ===
+
+console.log("2"=== 2);
