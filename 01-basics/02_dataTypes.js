b2e0b2521408a0f03d6fae57a8644aaa037f0396
MdZaid11 <109697860+MdZaid11@users.noreply.github.com>
2025-01-29 18:57:17 +0000

    dataType_Commit

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+// ---> Primitive Data Types
+
+ // number 
+ // bigint
+ // string
+ // boolean
+ // null  ===> standalone value
+ // undefined ===> abhi value assign nhi hui hai
+ // symbol ===> for unniqueness 
+
+
+ console.log(typeof "Zaid");
+ console.log(typeof null); //--> object
+ console.log(typeof undefined); //-->undefined 
+ console.log(typeof 1);
+
+ 
+
+ 
