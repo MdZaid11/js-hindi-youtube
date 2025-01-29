8efd72d336cf42c0bba45e59b3365e7ac934460f
MdZaid11 <109697860+MdZaid11@users.noreply.github.com>
2025-01-29 19:50:46 +0000

    ConversionUpdated

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -27,3 +27,24 @@ let someNumber= 33
 let stringNumber= String(someNumber);
 
 console.log(typeof stringNumber);
+
+// ************** Operations *****************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+let str1="hello"
+let str2=" Zaid"
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log(typeof ("1"+2))
+console.log(typeof (1+"2"))
+
+console.log("1"+2+2);
+console.log(1+2+"2");
