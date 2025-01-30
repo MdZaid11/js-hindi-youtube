b45692caab9fa30af7f1ad2c6e3ed2a3aec5c4e4
MdZaid11 <109697860+MdZaid11@users.noreply.github.com>
2025-01-30 12:20:40 +0000

    returnType

---
+++ 01-basics/dataTypes.js
@@ -0,0 +1,20 @@
+//primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JavaScript --> JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. 
+// Reference (Not Primtive)
+
+// Array, Objects, Functions
+
+let obj= {
+    name:"Zaid",
+    age:22,
+    gender:"Male"
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
