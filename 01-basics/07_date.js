b445d3d5b344cbce0f9d0cc759e050526540a0a7
MdZaid11 <109697860+MdZaid11@users.noreply.github.com>
2025-01-30 20:17:50 +0000

    Dateandtime

---
+++ 01-basics/07_date.js
@@ -0,0 +1,19 @@
+ 
+
+ let myDate=new Date();
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toLocaleString());
+
+ 
+let myCreatedDate=new Date(2024, 0 , 23)
+console.log(myCreatedDate.toDateString());
+
+let newCreatedDate=new Date("2023-01-14");
+console.log(newCreatedDate.toDateString());
+
+let myTimeStamp=Date.now();
+console.log(myTimeStamp)
+console.log(newCreatedDate.getTime())
+
+ 
